8b585826795270379c562ca215241252e3649df2
zxc471313304 <52726197+zxc471313304@users.noreply.github.com>
2019-08-09 12:05:55 +0800

    创建怪物状态信息

--- 草原历险记/Assets/Scripts/EBunny_Status.cs
+++ 草原历险记/Assets/Scripts/EBunny_Status.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EBunny_Status : MonoBehaviour
 {
-    public float health = 10;
+    public float health = 100;
     private bool isDead = false;
     private Animation animation;
     // Start is called before the first frame update
